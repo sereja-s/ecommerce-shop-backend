eb1145bb2466c47d783542b60108489854733052
sereja-s <serejasuvorov1979@gmail.com>
2024-04-20 09:59:56 +0300

    Product, category, UserServiceImplementation

---
+++ src/main/java/com/zosh/service/UserServiceImplementation.java
@@ -0,0 +1,46 @@
+package com.zosh.service;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.zosh.config.JwtProvider;
+import com.zosh.exception.UserException;
+import com.zosh.model.User;
+import com.zosh.repository.UserRepository;
+
+@Service
+public class UserServiceImplementation implements UserService {
+
+	private UserRepository userRepository;
+	private JwtProvider jwtProvider;
+
+	public UserServiceImplementation(UserRepository userRepository, JwtProvider jwtProvider) {
+		this.userRepository = userRepository;
+		this.jwtProvider = jwtProvider;
+	}
+
+	@Override
+	public User findUserById(Long userId) throws UserException {
+
+		Optional<User> user = userRepository.findById(userId);
+		if (user.isPresent()) {
+			return user.get();
+		}
+		throw new UserException("user not found with id: " + userId);
+	}
+
+	@Override
+	public User findUserProfileByJwt(String jwt) throws UserException {
+
+		String email = jwtProvider.getEmailFromToken(jwt);
+
+		User user = userRepository.findByEmail(email);
+
+		if (user == null) {
+			throw new UserException("user not found with email " + email);
+		}
+		return user;
+	}
+
+}
